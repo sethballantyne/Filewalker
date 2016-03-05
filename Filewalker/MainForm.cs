b65c2e32739910ce9248ffec61366e7a8aa98862
Seth Ballantyne <seth.ballantyne@gmail.com>
2016-03-05 13:34:34 +1300

    Licence and bug fix
    - The licence really has been added to all source files this time. :-p
    - Fixed a bug where the application would crash when the icon for any
    given file couldn't be retrieved.

--- Filewalker/MainForm.cs
+++ Filewalker/MainForm.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*Copyright (c) 2016 Seth Ballantyne <seth.ballantyne@gmail.com>
+*
+*Permission is hereby granted, free of charge, to any person obtaining a copy
+*of this software and associated documentation files (the "Software"), to deal
+*in the Software without restriction, including without limitation the rights
+*to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*copies of the Software, and to permit persons to whom the Software is
+*furnished to do so, subject to the following conditions:
+*
+*The above copyright notice and this permission notice shall be included in
+*all copies or substantial portions of the Software.
+*
+*THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*THE SOFTWARE.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -85,7 +106,11 @@ namespace Filewalker
                 if(imageList.Images[fileExtension] == null)
                 {
                     Icon associatedIcon = ShellIcon.GetSmallIcon(file.DirectoryName + "\\" + file.Name);
-                    imageList.Images.Add(fileExtension, associatedIcon);
+                    
+                    // make sure we actually retrieved the files icon; passing a null
+                    // value to the image list will result in an exception being thrown.
+                    if (associatedIcon != null)
+                        imageList.Images.Add(fileExtension, associatedIcon);
                 }
                 
                 //listViewItems.Add(new ListViewItem()
